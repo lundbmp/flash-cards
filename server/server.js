1fb44ba9ec65e5b1e12fd60226266139eaba40ab
lundbmp <m.lundberg008@gmail.com>
2022-09-22 10:42:50 -0500

    figured out dependency issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const mongoose = require('mongoose');
 
-//const db = require("./config/connection");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,14 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pizza-hunt",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-app.listen(PORT, () => {
-  console.log(`listenting on ${PORT}...`);
+db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`listenting on ${PORT}...`);
+    });
 });
