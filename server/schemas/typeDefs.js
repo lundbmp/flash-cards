c446f974db6aff0c4e659869fd390cc533399e4c
Deadpool1 <atwood169@yahoo.com>
2022-09-27 19:17:12 -0500

    set up schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+
+    }
+
+    type Card {
+
+    }
+
+    type Auth {
+
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+
+    }
+`;
+
+module.exports = typeDefs;
