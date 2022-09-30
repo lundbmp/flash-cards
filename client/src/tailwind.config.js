812ee5f656d1cd1d835229a389caaa2fb22fe789
Raquel Stankey <brv1114@yahoo.com>
2022-09-30 14:57:47 -0500

    working on tailwind

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,8 +1,35 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
+
+//@type {import('tailwindcss').Config}
+
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// module.exports = {
+//   theme: {
+//     spacing: {
+//       1: "8px",
+//       2: "12px",
+//       3: "16px",
+//       4: "24px",
+//       5: "32px",
+//       6: "48px",
+//     },
+//   },
+// };
