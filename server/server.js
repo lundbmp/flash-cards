02828f7f7d44c89b780f577c9109c4fdfcee64fc
lundbmp <m.lundberg008@gmail.com>
2022-10-02 16:43:49 -0500

    trying to use link

--- server/server.js
+++ server/server.js
@@ -29,8 +29,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
     app.use(express.static(path.join(__dirname, "../client/build")));
   }
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  app.get("*", (res, req) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 
   db.once("open", () => {
