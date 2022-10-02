359553bbce0f9aaf62805f95b95abc1a7d5022aa
Raquel Stankey <brv1114@yahoo.com>
2022-10-01 20:01:16 -0500

    pushing styling updates to home page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -23,14 +23,15 @@
 export default function Dashboard() {
   return (
     <main>
-      <div className="grid sm:grid-cols-1 sm:grid-cols-1 h-screen">
+      <p>Create a Flash Card Below:</p>
+      <div className="grid sm:grid-cols-1 h-screen">
         <div className="bg-gray-800 flex flex-col justify-center">
-          <form className="max-w-[400px] w-96 mx-auto bg-gray-900 p-8 px-8 rounded-lg">
+          <form className="max-w-[400px] mx-auto bg-gray-900 p-8 px-8 rounded-lg">
             <h2 className="text-4xl dark:text-white font-bold text-center">
-              LOGIN
+              Flash Card
             </h2>
             <div className="flex flex-col text-gray-400 py-2">
-              <label>Email</label>
+              <label></label>
               <input
                 className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
                 type="text"
