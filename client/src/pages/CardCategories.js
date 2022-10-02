f31c9c0474e728db1bbe6f43a51a62c8a0de8fef
lundbmp <m.lundberg008@gmail.com>
2022-10-02 14:56:18 -0500

    display list of cards on card categories

--- client/src/pages/CardCategories.js
+++ client/src/pages/CardCategories.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 
-const CardCategories = ({category}) => {
-    return (
-        <div>{category}</div>
-    );
-}
+import CardList from "../components/CardList";
+
+import { getCategoryCards } from "../utils/queries";
+
+const CardCategories = ({ category }) => {
+  const { loading, error, data } = useQuery(getCategoryCards, {
+    variables: { category: category },
+  });
+
+  if(loading) return <div>Loading</div>;
+  if(error) return `error: ${error}`;
+
+  const cards = data.getCategoryCards;
+  
+  return (
+    <div>
+    <CardList cards={cards} />
+    </div>
+  );
+};
 
 export default CardCategories;
